cd02056e9f91b0ce8f1341b60d16003a6c50c013
fbinna <fbinna@hsr.ch>
2014-10-16 17:03:18 +0200

    System Style im GadgetMasterTest

--- src/view/GadgetMasterViewTest.java
+++ src/view/GadgetMasterViewTest.java
@@ -1,9 +1,29 @@
 package view;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class GadgetMasterViewTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		GadgetMasterView blubb = new GadgetMasterView();
 		blubb.setVisible(true);
 		
